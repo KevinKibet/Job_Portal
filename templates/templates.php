0b8dc2518b2d35b11e36633d24bee53f074b7a7d
Kevin Kibet <kevinkipkosgei310@gmail.com>
2020-04-11 09:37:18 +0300

    template

---
+++ templates/templates.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" type="text/css" href="">
+<title></title>
+<body>
+	<ul>
+		<li>Home</li>
+		<li>Jobs Search</li>
+		<li>Popular Jobs</li>
+		<li>Hiring Now</li>
+		<li>About</li>
+		<li>Contact </li>	
+	</ul>
+</body>
+</html>
